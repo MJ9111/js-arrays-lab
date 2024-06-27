c6fcac8f51fe0d7be9955d51b3a14433a2337180
mj9111 <1191.1mj91.1@gmail.com>
2024-06-27 08:47:33 +0300

    Fisrt commit

---
+++ app.js
@@ -0,0 +1,50 @@
+
+//const foods = ['taco', 'pizza', 'cheesburger'];
+
+//const allfoods = foods.join(` -> `);
+//const hassoup = foods.includes(`soup`);
+
+//console.log(hassoup);
+
+//const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90];
+
+//const odds = [];
+
+//for(
+//    let i = 0; i < nums.length; i++) {
+//{
+//    odds.push(nums[i]);
+//}
+   // }
+
+ //   console.log(odds);
+
+// const nums = [1, 3, 5 ,9 ,15 ,25 ,30 ,45 ,50];
+// const fizz = [];
+// const buzz = [];
+// const fixxbuzz = [];
+
+// for (let i =0; i < nums.length; i++) {
+//if (nums[i] % 3 === 0 & nums[i] % 5 === 0) {
+//    fixxbuzz.push(nums[i]);
+
+//}else if (nums[i] % 3 === 0){
+//    fizz.push(nums[i]);
+//}else if (nums[i] % 5=== 0){
+//    buzz.push(nums[i]);
+//}
+
+// }
+ //console.log( `fizz:`, fizz);
+ //console.log(`buzz:` ,buzz);
+ //console.log(`fixxbuzz:`, fixxbuzz);
+
+ const numsarrays = [
+[100 ,5 ,23],
+[15, 21, 72, 9]
+[7 , 81 ,90]
+ ];
+
+ const numlist = numsarrays.slice(-1)[0];
+
+ console.log(numlist);
