9885ad2d87ad4b599313e7fed8c2fae49738e741
mj9111 <1191.1mj91.1@gmail.com>
2024-06-30 08:49:38 +0300

    correction

--- app.js
+++ app.js
@@ -6,6 +6,157 @@
 
 //console.log(hassoup);
 
+
+ //const foods =[];
+
+ //console.log(foods);
+
+//const foods =[];
+
+//foods.pusg('pizza');
+//foods.push('cheeseburger');
+
+//console.log(foods);
+
+
+ //const foods =[];
+
+ //console.log(foods);
+
+//const foods =[];
+
+//foods.pusg('pizza');
+//foods.push('cheeseburger');
+
+//foods.unshift('taco');
+
+//console.log(foods);
+
+/*
+Exercise 4: Access an array element
+
+1) Retrieve the 'pizza' string from the array based on its position (index) in
+   the array.  
+
+2) Assign it to a variable called `favFood`.
+
+Complete Exercise 4 in the space below:
+*/
+
+
+
+//const foods = [];  
+
+//foods.push('pizza');
+//foods.push('cheeseburger');
+
+//foods.unshift('taco');
+
+//const favFood = foods[1];
+
+//console.log('Exercise 4 result:', favFood);
+
+//const foods = [];  
+
+//foods.push('pizza');
+
+//foods.push('cheeseburger');
+
+///foods.unshift('taco');
+
+//foods.splice(2, 0, 'tofu');
+
+//console.log('Exercise 5 result:', foods);
+
+/*
+Exercise 6: Replace elements
+
+1) Replace 'pizza' in the `foods` array with 'sushi' and 'cupcake'.
+
+Complete Exercise 6 in the space below:
+*/
+
+
+
+//console.log('Exercise 6 result:', foods);
+
+//const foods = [];  
+
+//foods.push('pizza');
+//foods.push('cheeseburger');
+
+//foods.unshift('taco');
+
+//foods.splice(2, 0, 'tofu');
+
+//foods.splice(1, 1, 'sushi', 'cupcake'); 
+
+//console.log('Exercise 6 result:', foods);
+
+//const foods = [];  
+
+//foods.push('pizza');
+//foods.push('cheeseburger');
+
+//foods.unshift('taco');
+//foods.splice(2, 0, 'tofu'); 
+ 
+//foods.splice(1, 1, 'sushi', 'cupcake'); 
+//const yummy = foods.slice(1, 3); 
+//console.log( yummy);
+
+//const foods = [];  
+
+//foods.push('pizza');
+//foods.push('cheeseburger');
+
+//foods.unshift('taco');
+
+//foods.splice(2, 0, 'tofu');
+//foods.splice(1, 1, 'sushi', 'cupcake'); 
+//const soyIdx = foods.indexOf('tofu');
+//console.log( soyIdx);
+
+
+//const foods = [];  
+
+
+//foods.push('pizza');
+//foods.push('cheeseburger');
+
+
+//foods.unshift('taco');
+
+
+//foods.splice(2, 0, 'tofu');
+
+
+//foods.splice(1, 1, 'sushi', 'cupcake');
+
+//const allFoods = foods.join(' -> '); 
+
+//console.log('Exercise 9 result:', allFoods);
+
+//const foods = [];  
+
+
+//foods.push('pizza');
+//foods.push('cheeseburger');
+
+
+//foods.unshift('taco');
+
+
+//foods.splice(2, 0, 'tofu');
+
+
+//foods.splice(1, 1, 'sushi', 'cupcake');
+
+
+//const hasSoup = foods.includes('soup'); 
+
+//console.log('Exercise 10 result:', hasSoup);
+
 //const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90];
 
 //const odds = [];
@@ -39,12 +190,61 @@
  //console.log(`buzz:` ,buzz);
  //console.log(`fixxbuzz:`, fixxbuzz);
 
- const numsarrays = [
-[100 ,5 ,23],
-[15, 21, 72, 9]
-[7 , 81 ,90]
- ];
+// const numsarrays = [
+//[100 ,5 ,23],
+//[15, 21, 72, 9]
+//[7 , 81 ,90]
+// ];
 
- const numlist = numsarrays.slice(-1)[0];
+ //const numlist = numsarrays.slice(-1)[0];
 
- console.log(numlist);
+
+// const numArrays = [
+//  [100, 5, 23],
+//  [15, 21, 72, 9],
+//  [45, 66],
+//  [7, 81, 90]
+//];
+
+
+//const numList = numArrays.slice(-1)[0];
+
+//console.log('Exercise 13 result:', numList);
+
+
+//const numArrays = [
+//  [100, 5, 23],
+///  [15, 21, 72, 9],
+//  [45, 66],
+//  [7, 81, 90]
+//];
+
+
+//let num;
+
+//for (let i = 0; i < numArrays.length; i++) {
+//  if (numArrays[i].includes(66)) {
+//      num = 66;
+//      break;
+//  }
+//}
+
+//console.log('Exercise 14 result:', num);
+
+
+//const numArrays = [
+//[100,5,23],
+//[15,21,72,9],
+//[45,66],
+//[7,81,90]
+
+//];
+
+//let total = 0;
+ 
+//for (let i =0; i < numArrays.length; i++) {
+//  for (let j = 0; j < numArrays[i].length; j++) {
+//    total += numArrays[i] [j];
+//  }
+//}
+//console.log('Exercise 15 result:\n', total);
